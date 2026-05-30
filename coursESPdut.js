b1905c64b845bb3883676a6cec910e939e8468d3
Muhammad Diop <nabidiop18@gmail.com>
2026-05-30 17:51:01 +0000

    ajout verification de connexion sur les cours esp dut

--- coursESPdut.js
+++ coursESPdut.js
@@ -30,7 +30,7 @@ async function AfficherCours(){
             ${p.description}
           </p>
           
-<a href="${p.lien}" class="inline-flex font-medium items-center text-fg-brand hover:underline"  target="_blank">
+<a href="${p.lien}" class="lien inline-flex font-medium items-center text-fg-brand hover:underline"  target="_blank">
         Consulter
         <svg class="w-4 h-4 ms-2 rtl:rotate-[270deg]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 14v4.833A1.166 1.166 0 0 1 16.833 20H5.167A1.167 1.167 0 0 1 4 18.833V7.167A1.166 1.166 0 0 1 5.167 6h4.618m4.447-2H20v5.768m-7.889 2.121 7.778-7.778"/></svg>
     </a>
@@ -187,3 +187,16 @@ pc.addEventListener("click", () => {
   affiche.innerHTML = "";
   cours_pc();
 })
+
+document.addEventListener("click", (e) => {
+  const token = localStorage.getItem("token");
+
+  const lien = e.target.closest(".lien");
+
+  if (lien) {
+    if (!token) {
+      e.preventDefault();
+      window.location.href = "connexion.html";
+    }
+  }
+});
